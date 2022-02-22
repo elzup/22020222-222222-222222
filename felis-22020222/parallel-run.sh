bb55f3ddc0e6a880383104b7dfef0ff54e32321a
elzup <guild0105@gmail.com>
2022-02-22 22:18:47 +0900

    update scirpt

--- felis-22020222/parallel-run.sh
+++ felis-22020222/parallel-run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p out
 
-for i in {1..8} ; do
+for i in {1..4} ; do
   ./target/release/felis-22020222 > out/out-$i.txt &
+  echo "$ ./target/release/felis-22020222 > out/out-$i.txt"
 done
--- felis-22020222/src/main.rs
+++ felis-22020222/src/main.rs
@@ -1,6 +1,9 @@
 use chrono::prelude::*;
 
 fn main() {
+    // n=1 720000 4s
+    // n=22 720000 40s
+    // for _ in 0..72000 {
     for _ in 0..720000 {
         println!("{}", Local::now());
     }
