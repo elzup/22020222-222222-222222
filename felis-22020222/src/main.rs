6a9d5509e33bf3a4a7ea90ed0ce6c9dfaf1d7c8e
elzup <guild0105@gmail.com>
2022-02-22 23:18:14 +0900

    super power up

--- felis-22020222/src/main.rs
+++ felis-22020222/src/main.rs
@@ -1,10 +1,17 @@
 use chrono::prelude::*;
 
+// n=1 720000 4s
+// n=22 720000 40s
+// for _ in 0..72000 {
+const L: usize = 720000;
+
 fn main() {
-    // n=1 720000 4s
-    // n=22 720000 40s
-    // for _ in 0..72000 {
-    for _ in 0..720000 {
-        println!("{}", Local::now());
-    }
+    println!("{}", Utc::now());
+
+    let res = (0..L).map(|_| Utc::now().timestamp_nanos());
+
+    let joined: String = res.map(|id| id.to_string() + "\n").collect();
+
+    println!("{}", joined);
+    println!("{}", Utc::now());
 }
